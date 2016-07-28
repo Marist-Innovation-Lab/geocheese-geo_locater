bcc2a56bb39844bc6aa90aad275eb2bd062525ce
Tienza <Piradon.Liengtiraphan1@gmail.com>
2016-07-28 10:16:57 -0400

    Added try/except incase of query failure

--- geo_cheeseV0.1/geo_cheese/backup_query.py
+++ geo_cheeseV0.1/geo_cheese/backup_query.py
@@ -13,21 +13,23 @@ def to_string(word):
 
 def query_(my_ip):
     #my_ip = urllib2.urlopen('http://ip.42.pl/raw').read()
+    try:
+        isp_data = urllib2.urlopen('https://www.whoismyisp.org/ip/' + my_ip).read()
+        clean_isp_data = BeautifulSoup(isp_data).text
+        # print(clean_isp_data)
 
-    isp_data = urllib2.urlopen('https://www.whoismyisp.org/ip/' + my_ip).read()
-    clean_isp_data = BeautifulSoup(isp_data).text
-    # print(clean_isp_data)
+        isp_name = re.findall('Who\sis\smy\sISP\?(.*)The\sInternet\s', clean_isp_data)
+        isp_host = re.findall('this\sIP\sis\'(.*)\'\.Other', clean_isp_data)
+        isp_ip = re.findall('IP\saddress\sis(\d+.\d+.\d+.\d+).\sThis', clean_isp_data)
 
-    isp_name = re.findall('Who\sis\smy\sISP\?(.*)The\sInternet\s', clean_isp_data)
-    isp_host = re.findall('this\sIP\sis\'(.*)\'\.Other', clean_isp_data)
-    isp_ip = re.findall('IP\saddress\sis(\d+.\d+.\d+.\d+).\sThis', clean_isp_data)
+        isp_info2 = {
+            'name': to_string(isp_name[0]),
+            'host': to_string(isp_host[0]),
+            'ip': to_string(isp_ip[0])
+        }
 
-    isp_info2 = {
-        'name': to_string(isp_name[0]),
-        'host': to_string(isp_host[0]),
-        'ip': to_string(isp_ip[0])
-    }
-
-    return isp_info2
+        return isp_info2
+    except:
+        print("Backup query failed or Connection Rejected...")
 
 #query_("114.149.196.91")
